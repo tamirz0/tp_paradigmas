c37867778b259db3e3af6a48cb2d7028604cc88c
Gonzalo Rodriguez <rodriguezgonzalo1204@gmail.com>
2025-07-07 16:08:17 -0300

    IngredienteBasico Tests Completo

--- sistema_crafteo/test/sistema_crafteo/objeto/IngredienteBasicoTest.java
+++ sistema_crafteo/test/sistema_crafteo/objeto/IngredienteBasicoTest.java
@@ -1,6 +1,11 @@
 package sistema_crafteo.objeto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,7 +16,7 @@ class IngredienteBasicoTest {
 	
 	@BeforeEach
 	void setUp() {
-		basico = new IngredienteBasico("Basico", "Descripcion");
+		basico = new IngredienteBasico("Madera", "Descripcion");
 	}
 	
 	@Test
@@ -20,5 +25,27 @@ class IngredienteBasicoTest {
 		assertEquals(0, actual);
 	}
 	
-
+    @Test
+    void toString_retornElFormatoCorrecto() {
+        assertEquals("Basico Madera", basico.toString());
+    }
+    
+    @Test
+    void esCrafteable_retornaFalse() {
+        assertFalse(basico.esCrafteable());
+    }
+    
+    @Test
+    void equals_conDistintoNombre_noSonIguales() {
+        IngredienteBasico distinto = new IngredienteBasico("Piedra", "Roca");
+        assertNotEquals(basico, distinto);
+    }
+    
+    @Test
+    void getIngredientesBasicos_devuelveMapaConSiMismo() {
+        Map<Item,Integer> mapa = basico.getIngredientesBasicos();
+        assertEquals(1, mapa.size());
+        assertTrue(mapa.containsKey(basico));
+        assertEquals(1, mapa.get(basico));
+    }
 }
